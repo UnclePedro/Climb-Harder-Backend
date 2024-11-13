efdf08a58d669a459b1bfcc07c587da9f9fba6b7
Uncle Pedro <peterforsyth10@gmail.com>
2024-11-13 18:13:13 +1000

    Tweak schema to have season name - This allows backend to auto generate season name

--- src/api/helpers/seasonsHelper.ts
+++ src/api/helpers/seasonsHelper.ts
@@ -8,17 +8,28 @@ export const getSeasons = async (userId: number) => {
 };
 
 export const newSeason = async (userId: number) => {
+  // Find the last season by userId, ordered by season number in descending order
   const lastSeason = await prisma.season.findFirst({
     where: { userId },
-    orderBy: { seasonNumber: "desc" },
+    orderBy: { number: "desc" },
   });
 
+  // Calculate the new season number
+  const newSeasonNumber = lastSeason ? lastSeason.number + 1 : 1;
+
+  // Create the new season with an incremented number and formatted name
   const newSeason = await prisma.season.create({
     data: {
-      seasonNumber: lastSeason ? lastSeason.seasonNumber + 1 : 1,
-      userId: userId,
+      number: newSeasonNumber,
+      name: `Season ${newSeasonNumber} - ${new Date().toLocaleString(
+        "default",
+        { month: "long" }
+      )} ${new Date().getFullYear()}`,
+      userId,
     },
   });
+
+  return newSeason;
 };
 
 export const deleteSeason = async (seasonId: number) => {
