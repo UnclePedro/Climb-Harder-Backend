b20addb088a1a81b34c2f2004ba27980f36af9c6
Uncle Pedro <peterforsyth10@gmail.com>
2025-01-17 12:50:09 +1000

    Adjust for prod

--- api/config/endpointConfig.ts
+++ api/config/endpointConfig.ts
@@ -1,10 +1,10 @@
 // https://climb-harder-api.peterforsyth.dev
 // http://localhost:8080
-export const backendUrl = "http://localhost:8080";
+export const backendUrl = "https://climb-harder-api.peterforsyth.de";
 
 // https://climb-harder.peterforsyth.dev
 // http://localhost:5173
-export const frontendUrl = "http://localhost:5173";
+export const frontendUrl = "https://climb-harder.peterforsyth.dev";
 
 // ".climb-harder.peterforsyth.dev" || undefined
 export const cookiesDomian = ".climb-harder.peterforsyth.dev";
